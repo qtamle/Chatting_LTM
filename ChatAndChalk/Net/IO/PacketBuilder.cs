0975aca124a2d99ab21fae7f4721a151f1f7dca9
Tôm <lequoctamle55@gmail.com>
2024-10-29 17:28:49 +0700

    Phat sung dau tien

---
+++ ChatAndChalk/Net/IO/PacketBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAndChalk_Client.Net.IO
+{
+    class PacketBuilder
+    {
+        MemoryStream _ms;
+        public PacketBuilder() 
+        { 
+            _ms = new MemoryStream();
+        }
+
+        public void WriteOpCode(byte opcode)
+        {
+            _ms.WriteByte(opcode);
+        }
+
+        public void WriteMessage(string msg)
+        {
+            var msgLegth = msg.Length;
+            _ms.Write(BitConverter.GetBytes(msgLegth));
+            _ms.Write(Encoding.ASCII.GetBytes(msg));
+        }
+
+        public byte[] GetPacketBytes()
+        {
+            return _ms.ToArray();
+        }
+    }
+}
---
+++ ChatAndChalk/Net/Sever.cs
@@ -0,0 +1,80 @@
+﻿using ChatAndChalk_Client.Net.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAndChalk_Client.Net
+{
+    class Sever
+    {
+        TcpClient _client;
+        public PacketReader PacketReader;
+        PacketBuilder _packetBuilder;
+
+        public event Action connectedEvent;
+        public event Action msgReceivedEvent;
+        public event Action userDisconnectEvent;
+
+        public Sever()
+        {
+            _client = new TcpClient();
+        }
+
+        public void ConnectToSever(string username)
+        {
+            if (!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+                PacketReader = new PacketReader(_client.GetStream());
+
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var connectPacket = new PacketBuilder();
+                    connectPacket.WriteOpCode(0);
+                    connectPacket.WriteMessage(username);
+                    _client.Client.Send(connectPacket.GetPacketBytes());
+                }
+                ReadPackets();
+
+            }
+        }
+
+        private void ReadPackets()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var opcode = PacketReader.ReadByte();
+                    switch (opcode)
+                    {
+                        case 1:
+                            connectedEvent?.Invoke();
+                            break;
+                        case 5:
+                            msgReceivedEvent?.Invoke();
+                            break;
+                        case 10:
+                            userDisconnectEvent?.Invoke();
+                            break;
+                        
+                        default:
+                            Console.WriteLine("ah yess.");
+                            break;
+                    }
+                }
+            });
+        }
+
+        public void SendMessageToSever(string message) 
+        {
+            var messagePacket = new PacketBuilder();
+            messagePacket.WriteOpCode(5);
+            messagePacket.WriteMessage(message);
+            _client.Client.Send(messagePacket.GetPacketBytes());
+        }
+    }
+}
